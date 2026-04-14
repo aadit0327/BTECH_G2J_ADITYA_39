35e52997345ff798a908669905526433b222c1cb
aadit0327 <25113cn074@glbitm.ac.in>
2026-04-14 13:44:25 +0530

    Create voting_criteria.c

---
+++ Conditions/voting_criteria.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() 
+{
+    int age;
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    if (age >= 18) 
+    {
+        printf("You are eligible to Vote\n");
+    } 
+    else 
+    {
+        printf("You are not eligible to Vote\n");
+    }
+
+    return 0;
+}
