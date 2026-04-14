702875a81a881480f8cd011250bef2929a9c3d0d
aadit0327 <25113cn074@glbitm.ac.in>
2026-04-14 15:23:39 +0530

    Create largest_among_three.c

---
+++ Conditions/largest_among_three.c
@@ -0,0 +1,24 @@
+# include <stdio.h>
+int main ()
+{
+    int a,b,c;
+    printf ("Enter number a:\n");
+    scanf("%d",&a);
+    printf("Enter number b:\n");
+    scanf("%d",&b);
+    printf("Enter number c:\n");
+    scanf("%d",&c);
+    if(a>b && a>c)
+    {
+    printf("A is the Greatest");
+    }
+    else if (b>a && b>c)
+    {
+        printf("B is the Greatest");
+    }
+    else
+    {
+        printf("C is the Greatest");
+    }
+    return 0;
+}
