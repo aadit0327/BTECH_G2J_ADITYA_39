1aec69dee6cc8d55c8819206a36b3498ac1f65bf
aadit0327 <25113cn074@glbitm.ac.in>
2026-04-14 14:02:57 +0530

    Create leap_year.c

---
+++ Conditions/leap_year.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main ()
+{
+    int Year;
+    printf("Enter any year : ");
+    scanf("%d",&Year);
+    if (Year%4==0 && Year%100!=0 || Year%400==0)
+    {
+        printf("%d is a Leap Year", Year);
+    }
+    else
+    {
+        printf("%d is not a Leap Year", Year);
+    }
+    return 0;
+}
